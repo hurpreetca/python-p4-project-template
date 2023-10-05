ff14924c071e7fcb5c12973620e688d550922c0c
Harpreet Singh <hurpreet@icloud.com>
2023-10-05 00:12:02 -0400

    Started working on schemas using ma in app.py

--- server/app.py
+++ server/app.py
@@ -5,11 +5,23 @@
 # Remote library imports
 from flask import request, request, make_response, jsonify, session
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
+from config import app, db, api, ma 
+from models import User, Discussion, Comment
 
-# Local imports
-from config import app, db, api
-# Add your model imports
 
+
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+    
+    id = ma.auto_field()
+    name = ma.auto_field()
+    email = ma.auto_field()
+    _password_hash = ma.auto_field()
+
+single_user_schema = UserSchema()
+plural_user_schema = UserSchema(many=True)
 
 # Views go here!
 
--- server/seed.py
+++ server/seed.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
-# Standard library imports
 from random import randint, choice as rc
-
-# Remote library imports
 from faker import Faker
-
-# Local imports
-from app import app
-from models import db
+from config import db, app  
+from models import User, Discussion, Comment
 
 if __name__ == '__main__':
     fake = Faker()
