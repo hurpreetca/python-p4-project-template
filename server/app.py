9d9910dc93c3af32f1043bcf0eea57b9e6922446
Harpreet Singh <hurpreet@icloud.com>
2023-10-07 14:26:11 -0400

    Added Signup and login buttons

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@
 from flask import request, request, make_response, jsonify, session
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
-from config import app, api, ma 
+from config import app, api, ma , db
 from models import User, Discussion, Comment, Tag
 from flask import request, session
 from flask_restful import Resource
@@ -77,8 +77,110 @@ class Signup(Resource):
 
         user = User(name= json.get("name"), email= json.get("email"))
 
+        errors = []
+        keys = ["name", "email", "password_hash"]
+
+        for key in keys:
+            if not json[key]:
+                errors.append(f"{key} is required")
 
 
+        if json.get("password_hash"):
+            #setter method(password_hash) = (password from client side)
+            user.password_hash = json.get("password_hash")
+            
+        else:
+            return {"errors": "Passwords do not match"}, 422
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+
+            session["user_id"] = user.id
+            return singular_user_schema.dump(user), 201
+
+        except IntegrityError as e:
+
+            if isinstance(e, (IntegrityError)):
+                for error in e.orig.args:
+                    if "UNIQUE" in error:
+                        errors.append("Email already taken. Please try again")
+
+            return {"errors": ["Errors"]}
+api.add_resource(Signup, "/signup")
+
+
+class Login(Resource):
+    def post(self):
+        json = request.get_json()
+        email = json.get("email")
+        password = json.get("password_hash")
+
+        if not email or not password:
+            return {"errors": ["Email and password are required"]}, 400
+
+  
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return {"errors": ["Email not found"]}, 404
+
+
+        if not user.authenticate(password):
+            return {"errors": ["Incorrect password"]}, 401
+
+        session["user_id"] = user.id
+        return singular_user_schema.dump(user), 200
+
+api.add_resource(Login, "/login")
+
+class CurrentUser(Resource):
+    def get(self):
+ 
+        user_id = session.get("user_id")
+
+    
+        if not user_id:
+            return {"errors": ["No user is currently logged in"]}, 401
+
+
+        user = User.query.get(user_id)
+
+
+        if not user:
+            return {"errors": ["User not found"]}, 404
+
+
+        return singular_user_schema.dump(user), 200
+
+api.add_resource(CurrentUser, "/current_user")
+
+
+class Logout(Resource):
+    def delete(self): 
+
+        session.pop("user_id", None)
+        return {"message": "Logged out successfully"}, 200
+
+api.add_resource(Logout, "/logout")
+
+class Discussions(Resource):
+    def get(self):
+        discussion_list = [singular_discussion_schema.dump(discussion) for discussion in Discussion.query.all()]
+        return {"discussion_list": discussion_list}, 200
+
+class DiscussionById(Resource):
+    def patch(self, id):
+        data = request.get_json()
+
+        discussion = Discussion.query.filter_by(Discussion.id == id).first()
+    pass
+
+    def delete(self, id):
+        discussion = Discussion.query.filter_by(Discussion.id == id).first()
+        if not discussion:
+            return {"error": "Discussion not found"}, 404
+        # for comments
 
 
 
