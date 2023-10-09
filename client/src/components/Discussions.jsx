26b23284b258b2a2dbc098b7a621ad0c0fcbbbc4
Harpreet Singh <hurpreet@icloud.com>
2023-10-08 20:08:32 -0400

    Working on post method in discussion by id

--- client/src/components/Discussions.jsx
+++ client/src/components/Discussions.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Discussions() {
+  let { id } = useParams();
+  const [discussions, setDiscussions] = useState([]);
+
+  useEffect(() => {
+    fetch("/discussions")
+      .then((res) => res.json())
+      .then(setDiscussions);
+  }, []);
+
+  const allDiscussions = discussions.map((discussion) => {});
   return <div>Discussions</div>;
 }
 
